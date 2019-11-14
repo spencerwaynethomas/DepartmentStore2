a1406b2afe8dd823e461db0da53c231136c7d401
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-11-14 09:02:40 -0700

    app controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  
   def index
     @items = @department.items
   end
@@ -8,8 +11,44 @@ class ItemsController < ApplicationController
 
   def new
     @item = @department.items.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+  @item = @department.items.new(item_params)
+  if @item.save
+    redirect_to [@department, @item]
+  else
+    render :new
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path(@department)
+  end
+
+  private
+
+  def set_department
+    @department = Department.find(params[department_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+
+  def items_params
+    params.require(:item).permit(:name, :body)
+  end
+
+
+
 end
+
+
+
